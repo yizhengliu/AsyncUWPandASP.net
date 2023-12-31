8242d7c59f4547daca6ff7cc99e59d83dd8cc36a
JiheiBot <2757174746@qq.com>
2023-12-31 00:10:16 +0000

    Completed project
    By using API template for creating ASP.Net app and UWP project by bulding xaml UI and corresponding async code. this achieves the UX friendly and powerful invaulvement of IO long request without blocking UI thread t.

---
+++ TestTimeConsumingOperationWebAPI/TestTimeConsumingOperationWebAPI/Controllers/TestLongOperationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestTimeConsumingOperationWebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestLongOperationController : ControllerBase
+    {
+        [HttpGet]
+        //task based asynchronous pattern
+        public async Task<IActionResult> Get() 
+        {
+            //pasue for 5s
+            await Task.Delay(5000);
+
+            return Content("Web API Long Operation Completed");
+        }
+    }
+}
